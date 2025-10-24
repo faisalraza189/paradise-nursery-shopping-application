41957a93b2a3b60b6ba35b64016b150c20619da0
Faisal Raza <fraza189@gmail.com>
2025-10-24 15:02:19 +0500

    Fix GitHub Pages routing by switching to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import LandingPage from './pages/LandingPage';
 import ProductListingPage from './pages/ProductListingPage';
